bd5add0ce9ec819453e34b8d812b4563d5ec342f
Arthur van der Wal <arthurvanderwal@gmail.com>
2020-10-08 05:36:50 +0900

    Intermediate commit

--- js/neural-network.js
+++ js/neural-network.js
@@ -1,5 +1,101 @@
+    class ActivationFunction
+    {
+        calculate(x)
+        {
+            return x;
+        }
+
+        derivative(x)
+        {
+            let result = [];
+
+            for (let i = 0; i < x.length; i++)
+            {
+                result.push(1.0);
+            }
+
+            return result;
+        }
+    }
+
+    class SigmoidActivationFunction extends ActivationFunction
+    {
+        calculate(x)
+        {
+            return x.map(e => 1.0 / (1.0 + Math.exp(-x)));
+        }
+
+        derivative(x)
+        {
+            return x.map(e => {
+                let sigmoid = this.calculate(x);
+
+                return sigmoid * (1 - sigmoid);
+            });
+        }
+    }
+
+    class ReluActivationFunction extends ActivationFunction
+    {
+        calculate(x)
+        {
+            let result = [];
+
+            for (let i = 0; i < x.length; i++)
+            {
+                result.push(Math.max(x[i], 0.0));
+            }
+
+            return result;
+        }
+
+        derivative(x)
+        {
+            let result = [];
+
+            for (let i = 0; i < x.length; i++)
+            {
+                result.push(x[i] >= 0.0 ? 1.0 : 0.0);
+            }
+
+            return result;
+        }
+    }
+
+    class SoftMaxActivationFunction extends ActivationFunction
+    {
+        calculate(x)
+        {
+            let shift = Math.max.apply(null, x);
+            let result = x.map((e) => Math.exp(e - shift));
+
+            let expSum = result.reduce((acc, cur) => acc + cur);
+            let oneOverExpSum = 1.0 / expSum;
+
+            result = result.map((e) => e * oneOverExpSum);
+
+            let resultSum = 0.0;
+
+            for (let i = 0; i < result.length; i++)
+            {
+                resultSum += result[i];
+            }
+
+            console.log(resultSum);
+
+            return result;
+        }
+
+        derivative(x)
+        {
+
+        }
+    }
+
     class Neuron extends Drawable
     {
+        _id = parseInt(Math.random() * 100000000);
+
         _inputNeurons = [];
         _outputNeurons = [];
 
@@ -9,6 +105,8 @@
         _weights = [];
         _activationFunction = null;
         _outputValue = 0.0;
+
+        _errorSignal = 0.0;
 
         constructor(layerIndex, activationFunction)
         {
@@ -37,20 +135,21 @@
 
         calculateOutput()
         {
-            this._outputValue = this._bias;
+            this._outputValue = 0.0;
 
             for (let i = 0; i < this._inputNeurons.length; i++)
             {
                 this._outputValue += this._weights[i] * this._inputNeurons[i]._outputValue;
             }
 
-            this._outputValue = this._activationFunction(this._outputValue);
+            this._outputValue = this._activationFunction.calculate([this._outputValue]);
         }
 
         draw(canvas, offsetX, offsetY)
         {
             context.beginPath();
-            canvas.arc(this._drawableX + offsetX, this._drawableY + offsetY, this._drawableRadius, 2.0 * Math.PI, false);
+            context.arc(this._drawableX + offsetX, this._drawableY + offsetY, this._drawableRadius, 2.0 * Math.PI, false);
+
             //context.lineWidth = 5;
             //context.strokeStyle = '#003300';
             context.stroke();
@@ -65,6 +164,7 @@
         }
     }
 
+    // Use https://machinelearningmastery.com/implement-backpropagation-algorithm-scratch-python/
     class NeuralNetwork extends Drawable
     {
         _neurons = [];
@@ -108,11 +208,11 @@
             super();
 
             this._neurons = [];
-            this._neurons[0] = this.createLayer(0, inputCount);
+            this._neurons[0] = this.createLayer(0, inputCount, null);
 
             for (let i = 0; i < hiddenLayerCount; i++)
             {
-                this._neurons[i + 1] = this.createLayer(i + 1, hiddenLayerNeuronCount, Neuron.relu);
+                this._neurons[i + 1] = this.createLayer(i + 1, hiddenLayerNeuronCount, new ReluActivationFunction() /*Neuron.relu*/);
 
                 // Loop over previous layer nodes
                 for (let j = 0; j < this._neurons[i].length; j++)
@@ -121,7 +221,7 @@
                 }
             }
 
-            this._neurons[hiddenLayerCount + 1] = this.createLayer(hiddenLayerCount + 1, outputCount, Neuron.sigmoid);
+            this._neurons[hiddenLayerCount + 1] = this.createLayer(hiddenLayerCount + 1, outputCount, new SigmoidActivationFunction()/*Neuron.sigmoid*/);
 
             for (let i = 0; i < this._neurons[hiddenLayerCount].length; i++)
             {
@@ -129,6 +229,27 @@
             }
 
             //console.log(this);
+        }
+
+        static random(lowerRange, upperRange)
+        {
+            let range = upperRange - lowerRange;
+
+            return Math.random() * range + lowerRange;
+        }
+
+        randomizeWeights(lowerRange, upperRange)
+        {
+            for (let i = 0; i < this._neurons.length; i++)
+            {
+                for (let j = 0; j < this._neurons[i].length; j++)
+                {
+                    for (let k = 0; k < this._neurons[i][j]._weights.length; k++)
+                    {
+                        this._neurons[i][j]._weights[k] = NeuralNetwork.random(lowerRange, upperRange);
+                    }
+                }
+            }
         }
 
         getLayers()
@@ -139,6 +260,31 @@
         getLayer(layerIndex)
         {
             return this._neurons[layerIndex];
+        }
+
+        getOutputLayerIndex()
+        {
+            return this._neurons.length - 1;
+        }
+
+        getErrorSignals()
+        {
+            let result = [];
+
+            // input layer doesn't have weights
+            for (let layer = 1; layer < this._neurons.length; layer++)
+            {
+                let layerErrorSignals = [];
+
+                for (let neuronIndex = 0; neuronIndex < this._neurons[layer].length; neuronIndex++)
+                {
+                    layerErrorSignals.push(this._neurons[layer][neuronIndex]._errorSignal);
+                }
+
+                result.push(layerErrorSignals);
+            }
+
+            return result;
         }
 
         getWeights()
@@ -196,6 +342,86 @@
             }
 
             return result;
+        }
+
+        resetErrorSignal()
+        {
+            for (let layer = 0; layer < this._neurons.length; layer++)
+            {
+                for (let neuronIndex = 0; neuronIndex < this._neurons[layer].length; neuronIndex++)
+                {
+                    this._neurons[layer][neuronIndex]._errorSignal = 0.0;
+                }
+            }
+        }
+
+        // both arguments are arrays with length == outputlayer.length
+        backpropagate(output, desiredOutput)
+        {
+            this.resetErrorSignal();
+
+            // Calculate error signal for output layer
+
+            for (let neuronIndex = 0; neuronIndex < this._neurons[this.getOutputLayerIndex()].length; neuronIndex++)
+            {
+                let neuron = this._neurons[this.getOutputLayerIndex()][neuronIndex];
+
+                neuron._errorSignal = (desiredOutput[neuronIndex] - output[neuronIndex]) * neuron._activationFunction.derivative([neuron._outputValue]);
+                //NeuralNetwork.MeanSquaredError(output[neuronIndex], desiredOutput[neuronIndex]);
+
+                //console.log(neuron._errorSignal);
+            }
+
+            // Back propagate error signal into hidden layers
+
+            for (let layer = this._neurons.length - 2; layer > 0; layer--)
+            {
+                //debugger;
+                for (let neuronIndex = 0; neuronIndex < this._neurons[layer].length; neuronIndex++)
+                {
+                    for (let outputNeuronIndex = 0; outputNeuronIndex < this._neurons[layer][neuronIndex]._outputNeurons.length; outputNeuronIndex++)
+                    {
+                        let neuron = this._neurons[layer][neuronIndex];
+                        let outputNeuron = neuron._outputNeurons[outputNeuronIndex];
+
+                        //console.log(outputNeuron._errorSignal);
+                        //console.log(outputNeuron._weights[neuronIndex]);
+                        //console.log(neuron._activationFunction);
+
+                        neuron._errorSignal += outputNeuron._errorSignal * outputNeuron._weights[neuronIndex] * neuron._activationFunction.derivative([neuron._outputValue]);
+                    }
+                    /*
+                    for (let weightIndex = 0; weightIndex < this._neurons[layer][neuronIndex]._weights.length; weightIndex++)
+                    {
+                        this._neurons[layer][neuronIndex]._weights[weightIndex] -= NeuralNetwork._learningRate * MSEForLayer;
+                    }
+                    */
+                }
+            }
+
+            console.log("ERRORS");
+            console.log(this.getErrorSignals());
+            debugger;
+
+            // Update weights
+
+            // weight = weight + learning_rate * error * input
+
+            for (let layer = 1; layer < this._neurons.length; layer++)
+            {
+                for (let neuronIndex = 0; neuronIndex < this._neurons[layer].length; neuronIndex++)
+                {
+                    for (let weightIndex = 0; weightIndex < this._neurons[layer][neuronIndex]._weights.length; weightIndex++)
+                    {
+                        let neuron = this._neurons[layer][neuronIndex];
+
+                        neuron._weights[weightIndex] += NeuralNetwork._learningRate * neuron._errorSignal * neuron._inputNeurons[weightIndex]._outputValue;
+                    }
+                }
+            }
+
+            //console.log("WEIGHTS");
+            //console.log(this.getWeights());
         }
 
         draw(canvas, offsetX, offsetY)
